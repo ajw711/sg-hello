d6e0d97c4e48b99c8cb5c7aa6e340af0b0e369c5
jw <ajw711@naver.com>
2024-09-26 13:33:40 +0900

    sg-hello first

---
+++ src/main/java/com/sghello/SghelloApplication.java
@@ -0,0 +1,13 @@
+package com.sghello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SghelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SghelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sghello/SghelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sghello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
